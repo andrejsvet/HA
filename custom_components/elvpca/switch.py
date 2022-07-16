89e634d0ccd8fc603f5e0c1331df586793b01239
andrejsvet <andrej_svetina@mac.com>
2022-07-16 06:49:29 +0200

    Update switch.py

--- custom_components/elvpca/switch.py
+++ custom_components/elvpca/switch.py
@@ -31,7 +31,7 @@ def setup_platform(
     serial_device = discovery_info["device"]
 
     try:
-        pca = pcapy.PCA(serial_device)
+        pca = PCA(serial_device)
         pca.open()
 
         entities = [SmartPlugSwitch(pca, device) for device in pca.get_devices()]
