09901a669b4a82a5f92bd617abc32fa246bcd10d
andrejsvet <andrej_svetina@mac.com>
2023-01-02 22:04:09 +0100

    Create _init_.py

---
+++ custom_components/rfid/_init_.py
@@ -0,0 +1,47 @@
+"""The Elv integration."""
+import voluptuous as vol
+
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME, CONF_PORT, CONF_DEVICE, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import discovery
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.typing import ConfigType
+
+DOMAIN = "elvpca"
+
+DEFAULT_DEVICE = "/dev/ttyUSB0"
+DEFAULT_PORT = "1883"
+
+ELVPCA_PLATFORMS = [Platform.SWITCH]
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {vol.Optional(CONF_DEVICE, default=DEFAULT_DEVICE): cv.string,
+            vol.Required(CONF_HOST): cv.string,
+            vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.string, 
+            vol.Optional(CONF_USERNAME): cv.string,
+            vol.Optional(CONF_PASSWORD): cv.string,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+
+def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the PCA switch platform."""
+    #{"device": config[DOMAIN][CONF_DEVICE]}
+    paramjson={}
+    paramjson["device"]= config[DOMAIN][CONF_DEVICE]
+    paramjson["host"]= config[DOMAIN][CONF_HOST]
+    paramjson["port"]= config[DOMAIN][CONF_PORT]
+    paramjson["username"]= config[DOMAIN][CONF_USERNAME]
+    paramjson["password"]= config[DOMAIN][CONF_PASSWORD]
+    
+    for platform in ELVPCA_PLATFORMS:
+        discovery.load_platform(
+            hass, platform, DOMAIN, paramjson, config
+        )
+
+    return True
